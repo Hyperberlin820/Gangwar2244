dd7b950c0bfc94b42933b8f1dd58ac7f7fb4a50f
Gangwar2244 <abhaygangwar5865@gmail.com>
2024-09-24 16:19:06 +0530

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -9,6 +9,14 @@ def index():
 def run():
     app.run(host='0.0.0.0',port=8080)
 
+def run():
+    app.run(host='0.0.0.0',port=1111)
+
+def run():
+    app.run(host='0.0.0.0',port=8888)
+
+
+
 def keep_alive():
     t = Thread(target=run)
     t.start()    
